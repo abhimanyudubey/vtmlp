d70ae98d23000008e469e285a858946e783b0a16
abhimanyudubey <abhimanyu1401@gmail.com>
2013-07-16 05:47:14 -0400

    Added install-ec2.sh
    Shell script to install Amazon EC2, as given in tutorial.

---
+++ shell/install-ec2.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#Install script for the StarCluster Management System.
+#Install Java
+echo $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+CDIR=$(pwd)
+echo "Current Working Directory $CDIR"
+if [ ! type java ]; then
+if type yum
+	then
+	sudo yum -y install java-1.6.0-openjdk
+else
+	if type apt-get
+	then
+	sudo apt-get install openjdk-6-jre
+	fi
+fi
+fi
+
+cd $CDIR
+#Install EC2 API and AMI Tools
+rm -f -r ~/.scm
+mkdir -p ~/.scm/ec2/tools
+curl -o /tmp/ec2-api-tools.zip http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip
+unzip /tmp/ec2-api-tools.zip -d /tmp
+cp -r /tmp/ec2-api-tools-*/* ~/.scm/ec2/tools
+curl -o /tmp/ec2-ami-tools.zip http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip
+unzip /tmp/ec2-ami-tools.zip -d /tmp
+cp -rf /tmp/ec2-ami-tools-*/* ~/.scm/ec2/tools
+echo export EC2_BASE=~/.scm/ec2 >> ~/.bashrc
+echo export EC2_HOME=~/.scm/ec2/tools >> ~/.bashrc
+echo export EC2_PRIVATE_KEY=~/.scm/ec2/certificates/ec2-pk.pem >> ~/.bashrc
+echo export EC2_CERT=~/.scm/ec2/certificates/ec2-cert.pem >> ~/.bashrc
+echo export EC2_URL=https://ec2.amazonaws.com >> ~/.bashrc
+echo export AWS_ACCOUNT_NUMBER=$3 >> ~/.bashrc
+echo export AWS_ACCESS_KEY_ID=$1 >> ~/.bashrc
+echo export AWS_SECRET_ACCESS_KEY=$2 >> ~/.bashrc
+echo export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:~/.scm/ec2/tools/bin >> ~/.bashrc
+echo export JAVA_HOME=/usr >> ~/.bashrc
+. ~/.bashrc
+if [ -f $4 ]
+	then
+	mkdir -p ~/.scm/ec2/certificates
+	cp $4 ~/.scm/ec2/certificates/ec2-cert.pem
+	echo "Certificate Written."
+else
+	if [ -f $CDIR/$4 ]
+	then
+		mkdir -p ~/.scm/ec2/certificates
+		cp $CDIR/$4 ~/.scm/ec2/certificates/ec2-cert.pem
+		echo "Certificate Written."
+	fi
+fi
+if [ -f $5 ]
+	then
+	mkdir -p ~/.scm/ec2/certificates
+	cp $5 ~/.scm/ec2/certificates/ec2-pk.pem
+	echo "S3 PrivateKey Written."
+else
+	if [ -f $CDIR/$5 ]
+	then
+		mkdir -p ~/.scm/ec2/certificates
+		cp $CDIR/$5 ~/.scm/ec2/certificates/ec2-pk.pem
+		echo "S3 PrivateKey Written."
+	fi
+fi
+if [ -f $7 ]
+	then
+	cp $7 ~/.scm/ec2/certificates/$6.pem
+	chmod 400 ~/.scm/ec2/certificates/$6.pem
+	echo "EC2 PrivateKey Written."
+else
+	if [ -f $CDIR/$7 ]
+	then
+		cp $CDIR/$7 ~/.scm/ec2/certificates/$6.pem
+		chmod 400 ~/.scm/ec2/certificates/$6.pem
+		echo "EC2 PrivateKey Written."
+	fi
+fi
+echo "Installation of EC2 Completed Successfully."
