cb72ab7a8ce71694cc26c44f15c1610c5e2e6058
abhimanyudubey <abhimanyu1401@gmail.com>
2013-07-22 08:13:18 -0400

    updated pascal_train.m

--- voc/compile_pascal_train.m
+++ voc/compile_pascal_train.m
@@ -1,38 +1,50 @@
+% Script to create executable for pascal_train.
+% Written by Abhimanyu Dubey for Dr. Dhruv Batra's Machine Learning and Perception Laboratory
+% Virginia Polytechnic Institute and State University, 2013.
+
+% Parameters during execution:
+%	out_path	: Path to save output.
+%	cls			: The class which has to be trained. (pascal_train)
+% 	n			: The number of aspect ratio clusters to use. (pascal_train)
+%	numworkers	: Number of workers in matlabpool to be created. If 0, no matlabpool is created.
+
 function compile_pascal_train(out_path, cls, n, numworkers)
+	%Preprocessing all variables taken in, all are strings. Adding note 'deployed' for all deployed calls.
+	note='deployed';
+	numworkers = str2num(numworkers);
+	n  = str2num(n);
 
-note='deployed';
-numworkers = str2num(numworkers);
-opts = str2num(opts);
-n  = str2num(n);
-
-if isdeployed
-	if opts == 1
-	addpath(genpath('~/voc-release5/VOC2007/'));
-	elseif opts == 2
-		addpath(genpath('~/voc-release5/VOC2007/VOCdevkit/VOC2007'));
+	%If numworkers = 0, do not start matlabpool (launching individual jobs on Amazon EC2.)
+	if numworkers ~= 0
+		%Creating a custom matlabpool profile from local, with a random name to avoid clashes.
+		myCluster = parcluster('local');
+		myCluster.NumWorkers = numworkers;
+		symbols = ['a':'z' 'A':'Z'];
+		MAX_ST_LENGTH = 10;
+		stLength = randi(MAX_ST_LENGTH);
+		nums = randi(numel(symbols),[1 stLength]);
+		x = symbols (nums);	
+		saveAsProfile(myCluster,x);
+		matlabpool('open',x,numworkers);
+		matlabpool size
+		%Prints size of matlabpool, should be equal to numworkers if matlabpool is created correctly.
 	end
-end
 
-if numworkers ~= 0
+	tStart = tic;
+	model = pascal_train(cls, n, note);
+	% Evaluates model.
+	tStop = toc(tStart);
+	%Save time of entire evaluation.
 
+	save(out_path,'model');
+	%Save the model to a .mat file.
 
+	timepath = strcat(out_path,'.time.txt');
+	save(timepath,'tStop','-ascii');
+	%Saving the run time to a text file with the same name as the out_path.
 
-myCluster = parcluster('local');
-myCluster.NumWorkers = numworkers;
-symbols = ['a':'z' 'A':'Z'];
-MAX_ST_LENGTH = 10;
-stLength = randi(MAX_ST_LENGTH);
-nums = randi(numel(symbols),[1 stLength]);
-x = symbols (nums);
-saveAsProfile(myCluster,x);
-matlabpool('open',x,numworkers);
-matlabpool size
-tic;
-model = pascal_train(cls, n, note);
-% Evaluates model.
-toc;
-save(out_path,'model');
-%Save the model to a .mat file.
-matlabpool('close',x,numworkers);
-
+	if numworkers ~= 0
+		%If the matlabpool is created, closing it.
+		matlabpool('close',x,numworkers);
+	end
 end
