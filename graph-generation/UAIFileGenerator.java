aed51c048c9d2d2a807fa03a7e75aa3c61a18929
abhimanyudubey <abhimanyu1401@gmail.com>
2013-07-22 18:18:24 -0400

    updated UAIFileGenerator

--- graph-generation/UAIFileGenerator.java
+++ graph-generation/UAIFileGenerator.java
@@ -2,81 +2,61 @@
  * Written by Abhimanyu Dubey for Dr. Dhruv Batra's Machine Learning and Perception Laboratory
  * Virginia Polytechnic Institute and State University, 2013. */
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.concurrent.ConcurrentHashMap;
 public class UAIFileGenerator {
 	public static void main(String args[])throws IOException{
 		PrintWriter out = new PrintWriter(args[0]);
+		PrintWriter outtable = new PrintWriter(args[0].concat("table.uai"));
 		//Destination for output file.
 		out.println("MARKOV");
 		int nVars = Integer.parseInt(args[1]);
 		//number of variables in UAI.<Edit>
 		out.println(nVars);
-		int cardLimit = 11;
-		//maximum cardinality for any variable present.
 		int cards[] = new int[nVars];
 		for(int i=0;i<nVars;i++){
-			cards[i]=(int) (2+Math.random()*(cardLimit-2));
+			cards[i]=10;
 			out.print(cards[i]+" ");
 		}
 		out.println();
-		int maxDegree = 2;
-		//This code generates only 1 degree and 2 degree cliques.
 		
-		//added a density factor for varying sparsity in the graph.
-		double density = 0.1;
-		
-		ConcurrentHashMap<double[],int[]> facs = new ConcurrentHashMap<double[],int[]>();
 		long tot = nVars + Long.parseLong(args[2]);
 		out.println(tot);
 		
 		for(int i=0;i<nVars;i++){
 			out.println("1 "+i);
-		}
-
-		for(int k=0;k<Integer.parseInt(args[2]);k++){
-			int i=(int)(Math.random()*nVars), j=(int)(Math.random()*nVars);
-			double sum=i+j,product=i*j,t2[] ={sum,product};
-				if(!facs.contains(t2) && i!=j ){
-					int[] t = {i,j};
-					out.println("2 "+t[0]+" "+t[1]);
-					facs.put(t2, t);
-					System.out.println("put item "+k);
-				}
-		}
-		ArrayList<int[]> facsn = new ArrayList<int[]>(facs.values());
-		//Completed the preamble generation.
-		
-		out.println();
-		for(int i=0;i<nVars;i++){
-			out.println(cards[i]);
-			float sum=0;
+			outtable.println(cards[i]);float sum=0;
 			for(int j=0;j<cards[i]-1;j++){
 				float tr = (float) (Math.random()*(1-sum));
 				sum+=tr;
-				out.print(tr+" ");	
+				outtable.print(tr+" ");	
 			}
-			out.println(1-sum+"\n");
+			outtable.println(1-sum+"\n");
 		}
 		
-		for(int kk=0;kk<facsn.size();kk++) {
-			int[] mat=facsn.get(kk);
-			int i=mat[0],j=mat[1];
-			int cardprod = cards[i]*cards[j];
-			out.println(cardprod);
-			for(int k=0;k<cards[i];k++){
-				float sum=0;
-				for(int l=0;l<cards[j]-1;l++){
-					float tr = (float) (Math.random()*(1-sum));						
-					sum+=tr;
-					out.print(tr+" ");
+		int faclimit = Integer.parseInt(args[2]);
+		for(int k=0;k<faclimit-100;k++){
+			int[] factors = new int[10];
+			int lim=k+1;
+			for(int i=0;i<10;i++) {
+				factors[i]=(int)(Math.random()*(faclimit-lim)*(0.5)+lim);
+				lim=factors[i]+1;
+				out.println("2 "+k+" "+factors[i]);
+				outtable.println("100");
+				for(int ck=0;ck<10;ck++){
+					float sum=0;
+					for(int l=0;l<10;l++){
+						float tr = (float) (Math.random()*(1-sum));						
+						sum+=tr;
+						outtable.print(tr+" ");
+					}
+					outtable.println(1-sum);
 				}
-				out.println(1-sum);
+				System.out.println("added factor table "+i+" for "+k);
+				outtable.println();
 			}
-			System.out.println("added factor table "+kk);
-			out.println();
 		}
+		
+		out.println();
 		out.close();
+		outtable.close();
 	}
 }
