5c8198f4575fca55f9523b56847162d0ef383a15
abhimanyudubey <abhimanyu1401@gmail.com>
2013-07-22 18:17:54 -0400

    pascal_test edited.

--- voc/compile_pascal_test.m
+++ voc/compile_pascal_test.m
@@ -1,39 +1,59 @@
-function compile_pascal_test(model_path, testset, year, numworkers)
-% Script to create executable for pascal_train.
+% Script to create executable for pascal_test.
+% Written by Abhimanyu Dubey for Dr. Dhruv Batra's Machine Learning and Perception Laboratory
+% Virginia Polytechnic Institute and State University, 2013.
+
 % Parameters during execution:
-% 	model_path	: Path to model. 
-%	out_path	: Path to save output.
-%	testset		: Test set data.
-% 	year		: Year of test data to use.
-%	suffix		: Suffix for pascal_test.
-startup;
-% Adding the paths to the executables.
-suffix='';
+%	model_path	: Path to the model to test.(.mat, generated from pascal_train executable).
+%	testset		: Test set to test against. (pascal_train)
+% 	year		: VOC data (year) for testing the data (pascal_train)
+%	numworkers	: Number of workers in matlabpool to be created. If 0, no matlabpool is created.
 
-myCluster = parcluster('local');
-myCluster.NumWorkers = numworkers;
-symbols = ['a':'z' 'A':'Z'];
-MAX_ST_LENGTH = 10;
-stLength = randi(MAX_ST_LENGTH);
-nums = randi(numel(symbols),[1 stLength]);
-x = symbols (nums);
-saveAsProfile(myCluster,x);
-matlabpool('open',x,numworkers);
-matlabpool size
-load(model_path,'-mat');
-% Loads model from .mat file.
+function compile_pascal_test(out_path, cls, n, numworkers, varargin)
+	%Preprocessing all variables taken in, all are strings. Adding note 'deployed' for all deployed calls.
+	suffix='deployed';
+	numworkers = str2num(numworkers);
+	n  = str2num(n);
 
-ds = pascal_test(model, testset, year, suffix); 
+	%If numworkers = 0, do not start matlabpool (launching individual jobs on Amazon EC2.)
+	if numworkers ~= 0
+		%Creating a custom matlabpool profile from local, with a random name to avoid clashes.
+		myCluster = parcluster('local');
+		myCluster.NumWorkers = numworkers;
+		symbols = ['a':'z' 'A':'Z'];
+		MAX_ST_LENGTH = 10;
+		stLength = randi(MAX_ST_LENGTH);
+		nums = randi(numel(symbols),[1 stLength]);
+		x = symbols (nums);	
+		saveAsProfile(myCluster,x);
+		matlabpool('open',x,numworkers);
+		matlabpool size
+		%Prints size of matlabpool, should be equal to numworkers if matlabpool is created correctly.
+	end
 
-ds
+	load(model_path,'-mat');
+	%Loads model from the .mat file.
+	tStart = tic;
+	nvarargs = length(varargin);
+	if nvarargs == 6
+		[annoPath, imgPath, imgsetPath, clsimgsetPath, clsresPath, detresPath] = varargin; 
+		ds = pascal_test(model, testset, year, suffix, annoPath, imgPath, imgsetPath, clsimgsetPath, clsresPath, detresPath);
+	else
+		ds = pascal_test(model, testset, year, suffix);
+	end
+	tStop = toc(tStart);
+	%Save time of entire evaluation.
+	ds
+	%Output of detections, for debugging.
 
-matlabpool('close',x,numworkers);
+	out_path = strcat(model_path,'.detections.mat');
+	save(out_path,'ds');
 
-out_path = strcat(model_path,'.detections.mat');
-save(out_path,'ds');
+	timepath = strcat(out_path,'.time.txt');
+	save(timepath,'tStop','-ascii');
+	%Saving the run time to a text file with the same name as the out_path.
 
-
-%Save the output to a .mat file.
-
-
+	if numworkers ~= 0
+		%If the matlabpool is created, closing it.
+		matlabpool('close',x,numworkers);
+	end
 end
