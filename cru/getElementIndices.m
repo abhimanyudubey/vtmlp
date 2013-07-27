457e57fdd28fdaf7768f23f8d022af431e83940a
abhimanyudubey <abhimanyu1401@gmail.com>
2013-07-27 18:19:49 -0400

    working indexing

--- cru/getElementIndices.m
+++ cru/getElementIndices.m
@@ -1,9 +1,9 @@
-function element = getElement(serial,graph)
+function element = getElementIndices(serial,graph)
     gsize = size(graph);
     sz = mod(serial,(gsize(1)*gsize(2)));
     z = (serial-sz)/(gsize(1)*gsize(2));
     sy = mod(sz,gsize(1));
     y = (sz-sy)/(gsize(1));
     x = sy;
-    element = graph(x+1,y+1,z+1);
+    element = [x+1,y+1,z+1];
 end
